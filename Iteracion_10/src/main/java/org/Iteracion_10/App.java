1e5359d990eea869f6833afdcd658a329f36855a
Agustin6199 <moramora00@hotmail.es>
2019-11-14 17:49:29 +0100

    Commit inicial con el proyecto multimódulo en Maven creado.

---
+++ Iteracion_10/src/main/java/org/Iteracion_10/App.java
@@ -0,0 +1,13 @@
+package org.Iteracion_10;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/ISO_C04/Pr/App.java
@@ -0,0 +1,13 @@
+package ISO_C04.Pr;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

dc3b3186ba25d10936dded04f03dae2d72787fe7
Agustin6199 <moramora00@hotmail.es>
2019-11-18 13:50:42 +0100

    Añadido el código correspondiente a la Iteración 1 del proyecto, basado en el correspondiente diagrama de Diseño.

---
+++ Iteracion_1/src/main/java/org/Iteracion_1/Gestor_Boxes.java
@@ -0,0 +1,10 @@
+package org.Iteracion_1;
+
+public class Gestor_Boxes {
+
+	public void CambiarEstado() {
+		// TODO - implement Gestor_Boxes.CambiarEstado
+		throw new UnsupportedOperationException();
+	}
+
+}

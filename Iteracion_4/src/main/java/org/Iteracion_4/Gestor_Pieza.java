d16555e71911fa20c7e73438352cfe5fc5349985
admin <admin@DESKTOP-5LP1VIK>
2019-11-21 19:20:05 +0100

    Se ha añadido el codigo correspondiente a la iteracion 4 basado en el diagrama de clases. Añadido las clases Gestor_Pieza y Pieza

---
+++ Iteracion_4/src/main/java/org/Iteracion_4/Gestor_Pieza.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package org.Iteracion_4;
+
+/**
+ * @author Alberto Sanchez
+ *
+ */
+public class Gestor_Pieza {
+	private char seleccionarPieza() {
+		// TODO - implement Gestor_Pieza.seleccionarPieza
+		throw new UnsupportedOperationException();
+	}
+
+}
---
+++ Iteracion_4/src/main/java/org/Iteracion_4/Pieza.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package org.Iteracion_4;
+
+/**
+ * @author Alberto Sanchez
+ *
+ */
+public class Pieza {
+	public char getNombre() {
+		// TODO - implement Pieza.getNombre
+		throw new UnsupportedOperationException();
+	}
+
+	public char getCodigo() {
+		// TODO - implement Pieza.getCodigo
+		throw new UnsupportedOperationException();
+	}
+
+}

7190bc8d613ce9509a003b34a1327f2bd614a5bd
Laura Muñoz Javega <Laura Muoz Javega@DESKTOP-MUJR7U9>
2019-11-26 18:00:41 +0100

    Añadida clases Gestor_estadisticas y Estadisticas necesarias para implementar la funcionalidad del Caso de Uso que corresponde a la iteración número 6.

---
+++ Iteracion_6/src/main/java/org/Iteracion_6/Estadisticas.java
@@ -0,0 +1,16 @@
+package org.Iteracion_6;
+
+public class Estadisticas {
+	 
+		private double tiempoMedio;
+
+		public double getTiempoMedio() {
+			return tiempoMedio;
+		}
+
+		public void setTiempoMedio(double tiempoMedio) {
+			this.tiempoMedio = tiempoMedio;
+		}
+		
+		
+}

acd8376bf361c9870a3c3c29b21c0994743b5a65
pitah <pitah@LAPTOP-891IKFBD>
2019-11-27 17:34:27 +0100

    Codigo correspondiente iteracion 7

--- Iteracion_7/src/main/java/org/Iteracion_7/Gestor_Cuenta.java
+++ Iteracion_7/src/main/java/org/Iteracion_7/Gestor_Cuenta.java
@@ -4,7 +4,7 @@ public class Gestor_Cuenta {
 	
 	
 
-		public void Imprmir_Cuenta() {
+		public void ImprmirCuenta() {
 			// TODO - implement Gestor_Box_Jefe.ReservarBox
 			throw new UnsupportedOperationException();
 		}

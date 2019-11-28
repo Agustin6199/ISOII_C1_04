56168b96be299d60e4f00acf772adbb180ed2cdc
admin <admin@DESKTOP-5LP1VIK>
2019-11-28 18:54:13 +0100

    Este codigo corresponde a la iteración 8. Se ha añadido la clase Gestor_boxes y se ha añadido un atributo a la Clase Box.

---
+++ Iteracion_8/src/main/java/org/Iteracion_8/Gestor_Tiempo_Box.java
@@ -0,0 +1,10 @@
+package org.Iteracion_8;
+
+public class Gestor_Tiempo_Box {
+
+	public void contarTiempo() {
+		// TODO - implement Gestor_Tiempo_Box.Actualizar
+		throw new UnsupportedOperationException();
+	}
+
+}
